90fe90c6980a47a3b2df6074f0f9a5ee1f8a6830
Galih Previand <galihpreviand27@gmail.com>
2023-09-13 22:24:26 +0700

    add page Item Detail

--- lib/ItemDetails.dart
+++ lib/ItemDetails.dart
@@ -1,10 +1,12 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:flutter_sertifikasi/models/item.dart';
+import 'package:intl/intl.dart';
 
 class ItemDetails extends StatelessWidget {
   final Item item;
 
-  const ItemDetails(this.item);
+  const ItemDetails(this.item, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,76 +14,82 @@ class ItemDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text('Item Details'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.blueGrey.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.all(16),
-                child: const Icon(
-                  Icons.ad_units,
-                  size: 100,
-                  color: Colors.blueGrey,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 300,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/test1.jpeg'),
+                  fit: BoxFit.contain,
                 ),
               ),
-              const SizedBox(height: 20),
-              const Text(
-                'Nama Barang:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    item.kode,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Deskripsi :',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  Text(
+                    item.deskripsi,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Harga Barang:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  Text(
+                    NumberFormat.currency(
+                      locale: 'en_US',
+                      symbol: 'Rp',
+                      decimalDigits: 2,
+                    ).format(item.price ?? 0),
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color:
+                          Colors.green, // Adjust the color to match your design
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  // Add more details as needed
+                ],
               ),
-              Text(
-                item.name,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Kode Barang:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
-                ),
-              ),
-              Text(
-                item.kode,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Harga Barang:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
-                ),
-              ),
-              Text(
-                'Rp. ${item.price?.toStringAsFixed(2)}', // Format price as currency
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              // Add more details as needed
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
